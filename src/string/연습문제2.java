0c09a0442e9f9e852ef082c1aaa88283c881cbfe
psc <dys02135@naver.com>
2023-05-24 20:12:09 +0900

    최종

--- src/string/연습문제2.java
+++ src/string/연습문제2.java
@@ -1,19 +1,31 @@
 package string;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class 연습문제2 {
 	public static void main(String[] args) {
 		
+		
 		String str = new String("Have a nice day Have a nice day Have a nice day");
-		int i=0;
-		ArrayList arr = new ArrayList();
-		while(i < str.length()) {
-			if(str.charAt(i) == 'a')
-				arr.add(i);
-			i++;
-		}
-		System.out.println(arr);
+	      int i=0;
+	      while(i < str.length()) {
+	         if(str.charAt(i) == 'a')
+	            System.out.print(i+", ");
+	         i++;
+	      }
+		
+		
+		
+//		
+//		String str = new String("Have a nice day Have a nice day Have a nice day");
+//		int i=0;
+//		ArrayList arr = new ArrayList();
+//		while(i < str.length()) {
+//			if(str.charAt(i) == 'a')
+//				arr.add(i);
+//			i++;
+//		}
+//		System.out.println(arr);
 		
 		
 		
